f259d282c1ff0457300620262621195563d1ba7c
Ефимов Александр <a.efimov@inenergy.ru>
2020-02-13 20:23:20 +0300

    simplify and beautify utils

--- src/utils/USBDetector.js
+++ src/utils/USBDetector.js
@@ -2,60 +2,31 @@ const usbDetect = require('usb-detection');
 const driveList = require('drivelist');
 const EventEmitter = require('events');
 
-class USBDetector extends EventEmitter {
-  constructor() {
-    super();
-    usbDetect.startMonitoring();
-    usbDetect.find(this._handleFoundDevices.bind(this));
-    this._listenAdd.bind(this)();
-    this._listenRemove.bind(this)();
-    this.stopMonitoring = usbDetect.stopMonitoring;
-  }
+const usbPort = new EventEmitter();
 
-  _handleFoundDevices(err, devices) {
-    const suitableDevice = devices.find(this._isSuitableDevice);
-    if (suitableDevice) {
-      this._findDrive(suitableDevice);
+usbDetect.startMonitoring();
+usbDetect.on('add', findDrive);
+usbDetect.on('remove', handleRemove);
+
+function findDrive() {
+  driveList.list().then(drives => {
+    const drive = drives.find(isSuitableDrive);
+    if (drive) {
+      usbPort.emit('add', drive.mountpoints[0].path);
     }
-  }
-
-  _listenAdd() {
-    usbDetect.on('add', (device) => {
-      if (this._isSuitableDevice(device)) {
-        setTimeout(this._findDrive.bind(this), 300, device);
-      }
-    });
-  }
-
-  _listenRemove() {
-    usbDetect.on('remove', (device) => {
-      if (JSON.stringify(this.connectedStorage) === JSON.stringify(device)) {
-        this.emit('remove');
-        this.connectedStorage = null;
-        this.storagePath = null;
-      }
-    });
-  }
-
-  _findDrive(device) {
-    driveList.list().then((drives) => {
-      const drive = drives.find(this._isSuitableDrive);
-      if (drive) {
-        this.connectedStorage = device;
-        this.storagePath = drive.mountpoints[0].path;
-        this.emit('connect', drive.mountpoints[0].path);
-      } else console.error('Drive not found');
-    });
-  }
-
-  _isSuitableDevice(device) {
-    let name = device.deviceName.toLowerCase();
-    return name.includes('storage') || name.includes('запоминающ');
-  }
-
-  _isSuitableDrive(drive) {
-    return !drive.isSystem && drive.isUSB && drive.isRemovable;
-  }
+  })
 }
 
-module.exports = new USBDetector();
+function isSuitableDrive(drive) {
+  return !drive.isSystem && drive.isUsb;
+}
+
+function handleRemove() {
+  driveList.list().then(drives => {
+    if (!drives.find(isSuitableDrive)) {
+      usbPort.emit('remove');
+    }
+  })
+}
+
+module.exports = usbPort;
--- src/utils/XLSLogger.js
+++ src/utils/XLSLogger.js
@@ -1,121 +1,66 @@
-const xl = require('excel4node');
-const path = require('path');
-const { INTEGRATED_PELTIER_PARAMS } = require('../constants');
+const xl = require("excel4node");
+const path = require("path");
 
-const wsColumns = {
-  hot: ['temperature', 'thermistor', 'thermoresistor', 'thermocouple'],
-  cool: ['temperature', 'thermistor', 'thermoresistor', 'thermocouple'],
-  peltier: ['current', 'voltage', 'deltaTemp'],
-  seebeck: ['current', 'voltage', 'deltaTemp'],
-};
+let wb, ws, fileName, headerStyle, dataStyle;
 
-const ADDITIONAL_PARAMS = {
-  deltaTemp: {
-    label: 'Разность температур',
-    units: '\u2103',
-  },
-};
-
-class XLSLogger {
-  constructor() {
-    this.fileName = this._createFileName();
-    this.workbook = new xl.Workbook();
-    this._addWorksheets();
-    this._createStyles();
-    this._fillTablesHeads();
-  }
-
-  writeToWorksheet(wsName, values) {
-    if (!this._validate(values)) return;
-    this._writeToWorksheet(this.worksheets[wsName], values);
-  }
-
-  saveLogTo(dir) {
-    this.workbook.write(path.join(dir, this.fileName));
-  }
-
-  _createFileName() {
-    const date = new Date();
-    return `ThermoElectricity_${date.getDate()}-${date.getMonth() +
-      1}-${date.getFullYear()}_${date.getHours()}-${date.getMinutes()}.xlsx`;
-  }
-
-  _fillTablesHeads() {
-    for (let n in wsColumns) {
-      for (let i = 0; i < wsColumns[n].length; i++) {
-        const entry =
-          INTEGRATED_PELTIER_PARAMS[wsColumns[n][i]] ||
-          ADDITIONAL_PARAMS[wsColumns[n][i]];
-        this.worksheets[n]
-          .cell(1, i + 1)
-          .string(entry.label + ', ' + entry.units)
-          .style(this.headerStyle);
-      }
-      this.worksheets[n].row++;
-    }
-  }
-
-  _writeToWorksheet(ws, values) {
-    for (let i = 0; i < values.length; i++) {
-      ws.cell(ws.row, i + 1)
-        .number(values[i])
-        .style(this.dataStyle);
-    }
-  }
-
-  _addWorksheets() {
-    this.worksheets = [
-      ['cool', 'Охлаждающаяся сторона'],
-      ['hot', 'Нагревающаяся сторона'],
-      ['peltier', 'Эффект Пелтье'],
-      ['seebeck', 'Эффект Зеебека'],
-    ].reduce(
-      (ws, [key, label]) => {
-        ws[key] = this.workbook.addWorksheet(label);
-        return ws;
-      },
-      { row: 1 }
-    );
-  }
-
-  _validate(values) {
-    for (let i = 0; i < values.length; i++) if (isNaN(values[i])) return;
-    return true;
-  }
-
-  _createStyles() {
-    this.headerStyle = this.workbook.createStyle({
-      font: {
-        bold: true,
-        color: 'ffffff',
-      },
-      fill: {
-        type: 'pattern',
-        patternType: 'solid',
-        fgColor: '8bc041',
-      },
-    });
-    this.headerStyle.border = this._generateBorders();
-    this.dataStyle = this.workbook.createStyle({
-      alignment: {
-        horizontal: 'right',
-      },
-    });
-    this.dataStyle.border = this._generateBorders();
-  }
-
-  _generateBorders() {
-    return ['left', 'right', 'top', 'bottom'].reduce(
-      (acc, key) => {
-        acc[key] = {
-          style: 'thin',
-          color: 'black',
-        };
-        return acc;
-      },
-      { outline: false }
-    );
+function createFile(fileName, headers) {
+  fileName = fileName;
+  wb = new xl.wb();
+  ws = wb.addWorksheet("Результаты");
+  if (!headerStyle) createStyles();
+  for (let i = 0; i < headers.length; i++) {
+    ws.cell(1, i + 1).string(headers[i]).style(headerStyle);
   }
 }
 
-module.exports = new XLSLogger();
+function writeRow(entries) {
+  for (let i = 0; i < entries.length; i++) {
+    ws.cell(row, i+1).number(entries[i]).style(dataStyle)
+  }
+}
+
+function saveFile(dir) {
+  wb.write(path.join(dir, fileName));
+  wb = ws = fileName = void 0;
+}
+
+function createStyles() {
+  headerStyle = wb.createStyle({
+    font: {
+      bold: true,
+      color: "ffffff"
+    },
+    fill: {
+      type: "pattern",
+      patternType: "solid",
+      fgColor: "8bc041"
+    }
+  });
+  headerStyle.border = generateBorders();
+  dataStyle = wb.createStyle({
+    alignment: {
+      horizontal: "right"
+    }
+  });
+  dataStyle.border = _generateBorders();
+}
+
+function generateBorders() {
+  return ["left", "right", "top", "bottom"].reduce(
+    (acc, key) => {
+      acc[key] = {
+        style: "thin",
+        color: "black"
+      };
+      return acc;
+    },
+    { outline: false }
+  );
+}
+
+module.exports = {
+  writeRow,
+  createFile,
+  saveFile
+};
+
