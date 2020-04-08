8944d2e7bb58216654eaf8199753e19fb4b9c10e
Alexander Efimov <efimokthefatass@gmail.com>
2020-04-08 04:32:11 +0300

    fix according to task

--- src/layouts/EffectsResearch.svelte
+++ src/layouts/EffectsResearch.svelte
@@ -1,27 +1,29 @@
 <script>
-  import RangeInput from '../molecules/RangeInput';
-  import Select from '../molecules/Select';
-  import Button from '../atoms/Button';
-  import { ipcRenderer } from 'electron';
-  import { COMMANDS, PELTIER_CONSTRAINTS } from '../constants';
-  import { data } from '../stores';
-  import Chart from 'chart.js';
-  import Zoom from 'chartjs-plugin-zoom';
-  import configureChart from './chart.config';
-  import { onMount } from 'svelte';
+  import RangeInput from "../molecules/RangeInput";
+  import Select from "../molecules/Select";
+  import Button from "../atoms/Button";
+  import SaveButton from "../organisms/SaveButton";
+  import { ipcRenderer } from "electron";
+  import { COMMANDS, PELTIER_CONSTRAINTS } from "../constants";
+  import { data } from "../stores";
+  import Chart from "chart.js";
+  import Zoom from "chartjs-plugin-zoom";
+  import configureChart from "./chart.config";
+  import { onMount, onDestroy } from "svelte";
   export let goBack;
 
   onMount(createChart);
+  onDestroy(() => chart && chart.destroy());
 
   function createChart() {
     chart = new Chart(
-      document.getElementById('chart').getContext('2d'),
+      document.getElementById("chart").getContext("2d"),
       configureChart(points, { x: xCaption, y: yCaption })
     );
     chart.options.onClick = chart.resetZoom();
   }
 
-  let saveDisabled=true,
+  let saveDisabled = true,
     startDisabled = true,
     chart,
     points = [],
@@ -31,26 +33,26 @@
   $: deltaTemp = $data.temperatureHot.value - $data.temperatureCool.value;
 
   ipcRenderer
-    .on('usbConnect', () => (saveDisabled = false))
-    .on('usbDisconnect', () => (saveDisabled = true));
+    .on("usbConnect", () => (saveDisabled = false))
+    .on("usbDisconnect", () => (saveDisabled = true));
 
   const effectsOptions = [
-    { label: 'Эффект Пельтье', value: 0 },
-    { label: 'Эффект Зеебека', value: 1 },
+    { label: "Эффект Пельтье", value: 0 },
+    { label: "Эффект Зеебека", value: 1 }
   ];
 
-  const I = 'I, A';
-  const deltaT = '\u0394T, \u02daC';
+  const I = "I, A";
+  const deltaT = "\u0394T, \u02daC";
 
   $: xCaption = selectedEffect.value ? deltaT : I;
   $: yCaption = selectedEffect.value ? I : deltaT;
 
-  $: updateAxis('x', xCaption);
-  $: updateAxis('y', yCaption);
+  $: updateAxis("x", xCaption);
+  $: updateAxis("y", yCaption);
 
   function updateAxis(axis, caption) {
     if (!chart) return;
-    chart.options.scales[axis + 'Axes'][0].scaleLabel.labelString = caption;
+    chart.options.scales[axis + "Axes"][0].scaleLabel.labelString = caption;
     chart.update();
   }
 
@@ -59,22 +61,21 @@
   function selectEffect(e) {
     startDisabled = false;
     selectedEffect = effectsOptions[e];
-    ipcRenderer.send('serialCommand', COMMANDS.turnOffAllPeltier);
+    ipcRenderer.send("serialCommand", COMMANDS.turnOffAllPeltier);
     if (e) {
-      ipcRenderer.send('serialCommand', COMMANDS.turnOnCoolPeltier);
-      ipcRenderer.send('serialCommand', COMMANDS.turnOnHotPeltier);
+      ipcRenderer.send("serialCommand", COMMANDS.turnOnCoolPeltier);
+      ipcRenderer.send("serialCommand", COMMANDS.turnOnHotPeltier);
     } else {
-      ipcRenderer.send('serialCommand', COMMANDS.turnOnProbePeltier);
+      ipcRenderer.send("serialCommand", COMMANDS.turnOnProbePeltier);
     }
   }
 
   function changePower(P) {
-    ipcRenderer.send('serialCommand', ...COMMANDS.setPowerCoolPeltier(P));
-    ipcRenderer.send('serialCommand', ...COMMANDS.setPowerHotPeltier(P));
+    ipcRenderer.send("serialCommand", ...COMMANDS.setPowerProbePeltier(P));
   }
 
   function changeCurrent(I) {
-    ipcRenderer.send('serialCommand', ...COMMANDS.setCurrentProbePeltier(I));
+    ipcRenderer.send("serialCommand", ...COMMANDS.setCurrentProbePeltier(I));
   }
 
   function toggleDrawing() {
@@ -90,11 +91,11 @@
 
   function startDrawing() {
     isDrawing = true;
-    let headers = ['\u0394T, \u2103', 'I, A'];
+    let headers = ["\u0394T, \u2103", "I, A"];
     if (!selectedEffect.value) headers = headers.reverse();
     ipcRenderer.send(
-      'createFile',
-      `TE-${selectedEffect.label.replace(' ', '-')}`,
+      "createFile",
+      `TE-${selectedEffect.label.replace(" ", "-")}`,
       headers
     );
     unsubscribeData = data.subscribe(addPoint);
@@ -103,14 +104,14 @@
   function addPoint(data) {
     const point = {
       x: selectedEffect.value ? deltaTemp : I,
-      y: !selectedEffect.value ? deltaTemp : I,
+      y: !selectedEffect.value ? deltaTemp : I
     };
     writeExcel(point);
     updateChart(point);
   }
 
   function writeExcel(point) {
-    ipcRenderer.send('excelRow', [point.x, point.y]);
+    ipcRenderer.send("excelRow", [point.x, point.y]);
   }
 
   function updateChart(point) {
@@ -119,68 +120,9 @@
   }
 
   function saveExcel() {
-    ipcRenderer.send('saveFile');
+    ipcRenderer.send("saveFile");
   }
 </script>
-
-<div class="layout">
-  <header>Исследование эффектов Пельтье и Зеебека</header>
-  <main>
-    <div class="controls">
-      <Select onChange={selectEffect} options={effectsOptions} />
-      <div class="param-temp">
-        <span class="temp-label">Температура нагревающейся пластины:</span>
-        <strong class="temp-value hot">
-          {$data.temperatureHot.value}{$data.temperatureHot.units}
-        </strong>
-      </div>
-      <div class="param-temp">
-        <span class="temp-label">Температура охлаждающейся пластины:</span>
-        <strong class="temp-value cool">
-          {$data.temperatureCool.value}{$data.temperatureCool.units}
-        </strong>
-      </div>
-      {#if selectedEffect.value}
-        <div class="range">
-          <span class="range-label">Мощность модуля Пелтье, % от макс</span>
-          <RangeInput
-            onChange={changePower}
-            range={PELTIER_CONSTRAINTS.PowerHot} />
-        </div>
-      {:else}
-        <div class="range">
-          <span class="range-label">
-            Установка тока, {$data.currentProbe.units}
-          </span>
-          <RangeInput
-            onChange={changeCurrent}
-            range={PELTIER_CONSTRAINTS.CurrentProbe} />
-        </div>
-      {/if}
-      <h3>Результаты измерений</h3>
-      {#each ['voltageProbe', 'currentProbe', 'deltaTemp'] as param}
-        <div class="result">
-          <span class="symbol">
-            {$data[param].symbol},
-            <em class="units">{$data[param].units}</em>
-            :
-          </span>
-          <strong class="value">{$data[param].value}</strong>
-        </div>
-      {/each}
-      <Button on:click={toggleDrawing} disabled={startDisabled}>{isDrawing ? 'Стоп' : 'Старт'}</Button>
-    </div>
-    <div class="chart">
-      <canvas id="chart" width="500" height="350" />
-    </div>
-  </main>
-  <footer>
-    <Button on:click={goBack}>Назад</Button>
-    <Button on:click={saveExcel} disabled={saveDisabled}>
-      Сохранить данные на usb-устройство
-    </Button>
-  </footer>
-</div>
 
 <style>
   main {
@@ -244,3 +186,62 @@
     font-size: 2rem;
   }
 </style>
+
+<div class="layout">
+  <header>Исследование эффектов Пельтье и Зеебека</header>
+  <main>
+    <div class="controls">
+      <Select onChange={selectEffect} options={effectsOptions} />
+      <div class="param-temp">
+        <span class="temp-label">Температура нагревающейся пластины:</span>
+        <strong class="temp-value hot">
+          {$data.temperatureHot.value}{$data.temperatureHot.units}
+        </strong>
+      </div>
+      <div class="param-temp">
+        <span class="temp-label">Температура охлаждающейся пластины:</span>
+        <strong class="temp-value cool">
+          {$data.temperatureCool.value}{$data.temperatureCool.units}
+        </strong>
+      </div>
+      {#if selectedEffect.value}
+        <div class="range">
+          <span class="range-label">Мощность модуля Пелтье, % от макс</span>
+          <RangeInput
+            onChange={changePower}
+            range={PELTIER_CONSTRAINTS.PowerProbe} />
+        </div>
+      {:else}
+        <div class="range">
+          <span class="range-label">
+            Установка тока, {$data.currentProbe.units}
+          </span>
+          <RangeInput
+            onChange={changeCurrent}
+            range={PELTIER_CONSTRAINTS.CurrentProbe} />
+        </div>
+      {/if}
+      <h3>Результаты измерений</h3>
+      {#each ['voltageProbe', 'currentProbe', 'deltaTemp'] as param}
+        <div class="result">
+          <span class="symbol">
+            {$data[param].symbol},
+            <em class="units">{$data[param].units}</em>
+            :
+          </span>
+          <strong class="value">{$data[param].value}</strong>
+        </div>
+      {/each}
+      <Button on:click={toggleDrawing} disabled={startDisabled}>
+        {isDrawing ? 'Стоп' : 'Старт'}
+      </Button>
+    </div>
+    <div class="chart">
+      <canvas id="chart" width="500" height="350" />
+    </div>
+  </main>
+  <footer>
+    <Button on:click={goBack}>Назад</Button>
+    <SaveButton onSave={saveExcel} disabled={saveDisabled} />
+  </footer>
+</div>
