85271df4d4e299b18224920bbc5a45721d539e9a
Ефимов Александр <a.efimov@inenergy.ru>
2020-06-23 20:10:17 +0300

    update build configuration

--- install.sh
+++ install.sh
@@ -1,39 +1,15 @@
-passwd --delete pi
-
-# WI-FI SETUP
-cat <<EOT >> /etc/wpa_supplicant/wpa_supplicant.conf
-network={
-    ssid="InEnergy"
-    psk="9031100133"
-}
-EOT
-wpa_cli -i wlan0 reconfigure
-
-# INSTALL REQUIRED PACKAGES
-apt install npm libudev-dev chromium-browser xorg
-
 # MAIN APP INSTALLATION
 npm i
 npm run build
 
-# MAIN APP AUTOSTART
-echo 'su -s /bin/bash -c startx pi&' > /etc/rc.local
-echo 'exit 0' > /etc/rc.local
-echo 'allowed_users=anybody' >> /etc/X11/Xwrapper.config
-echo '/opt/hydrogen-enrgy/HydrogenEnergy' > ~/.xinitrc
+sudo mkdir /usr/share/thermo-electricity
+cp -rf dist/linux-armv7l-unpacked/** /usr/share/thermo-electricity/
+echo '/usr/share/thermo-electricity/thermo-electricity-ui' > ~/.xinitrc
 chmod +x ~/.xinitrc
 
 
 # BOOT SPEED OPTIMIZATIONS
 cat <<EOT >> /boot/config.txt
-max_usb_current=1
-hdmi_group=2
-hdmi_mode=87
-hdmi_cvt 1024 600 60 6 0 0 0
-hdmi_drive=1
-
-enable_uart=1
-
 # Disable the rainbow splash screen
 disable_splash=1
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,62 @@
-const path = require("path");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const mode = process.env.NODE_ENV || "development";
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "renderer.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'renderer.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
     // see below for an explanation
     alias: {
-      svelte: path.resolve("node_modules", "svelte")
+      svelte: path.resolve('node_modules', 'svelte'),
     },
-    extensions: [".mjs", ".js", ".svelte"],
-    mainFields: ["svelte", "browser", "module", "main"]
+    extensions: ['.mjs', '.js', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main'],
   },
   module: {
     rules: [
       {
         test: /\.svelte$/,
         use: {
-          loader: "svelte-loader",
+          loader: 'svelte-loader',
           options: {
             emitCss: true,
             hotReload: true,
-            preserveWhitespace: false
-          }
-        }
+            preserveWhitespace: false,
+          },
+        },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: [
+          prod ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
   },
   externals: [
-    function(context, request, callback) {
+    function (context, request, callback) {
       if (
         /^path|fs|events|electron|drivelist|excel4node|usb-detection$/.test(
           request
         )
       ) {
-        return callback(null, "commonjs " + request);
+        return callback(null, 'commonjs ' + request);
       }
       callback();
-    }
+    },
   ],
-  mode
+  target: 'electron-renderer',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'svelte.css',
+    }),
+  ],
+  mode,
 };
