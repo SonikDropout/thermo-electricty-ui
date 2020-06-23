d75e3d4e0d41cede4c4836191fa8b2c7ff998883
Ефимов Александр <a.efimov@inenergy.ru>
2020-06-23 20:18:34 +0300

    fix permission error

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ npm i
 npm run build
 
 sudo mkdir /usr/share/thermo-electricity
-cp -rf dist/linux-armv7l-unpacked/** /usr/share/thermo-electricity/
+sudo cp -rf dist/linux-armv7l-unpacked/** /usr/share/thermo-electricity/
 echo '/usr/share/thermo-electricity/thermo-electricity-ui' > ~/.xinitrc
 chmod +x ~/.xinitrc
 
