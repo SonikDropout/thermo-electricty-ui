5582eb72042ef4de9f076464e1d1bfc3d4f86738
Ефимов Александр <a.efimov@inenergy.ru>
2020-08-28 11:24:57 +0300

    add installation instruction

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 npm i &&
 npm run build &&
 
-sudo mkdir /usr/share/thermo-electricity
-sudo cp -rf dist/linux-armv7l-unpacked/** /usr/share/thermo-electricity/
-echo '/usr/share/thermo-electricity/thermo-electricity-ui' > ~/.xinitrc
-chmod +x ~/.xinitrc
+sudo mkdir /usr/share/thermo-electricity &&
+sudo cp -rf dist/linux-armv7l-unpacked/** /usr/share/thermo-electricity/ &&
+echo '/usr/share/thermo-electricity/thermo-electricity-ui' > ~/.xinitrc &&
+chmod +x ~/.xinitrc &&
 
-while read p; do sudo systemctl disable "$p"; done < unused-packages.list
+while read p; do sudo systemctl disable "$p"; done < unused-packages.list &&
+
+reboot
