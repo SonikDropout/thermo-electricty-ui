ab302b84457faf5957c6cd3a7472a800b6fc7873
Ефимов Александр <a.efimov@inenergy.ru>
2021-03-12 15:24:54 +0300

    fix critical error

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -25,7 +25,7 @@ module.exports = function parse(buf) {
   for (const key in ps) {
     ps[key].value = buf[i++];
   }
-  for (let key in ['Cool', 'Hot', 'Probe']) {
+  for (let key in ['Cool', 'Hot']) {
     pp['thermocouple' + key].value *= THERMOCOUPLE_COEFFICIENT;
   }
   return { ...pp, ...ps };
